d9f8ec8b3fe8dcff751f2a1608f5d0da7adf0472
Sai Monish Tunguturu <tsaimonish@gmail.com>
2019-08-18 15:15:41 -0700

    homework assignment 8/18/2019

---
+++ appendHTML.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homework Assignment</title>
+</head>
+<body>
+    <p>homework assignment for 8/18/2019 (Append HTMl contents using JQuery)</p>
+</body>
+</html>
